dbee65435a32dabf8fbdaab77604fdef3411e30c
datre9 <david.trembacz@gmail.com>
2024-04-28 21:17:51 +0200

    Implement creating items in frontend

--- src/main/frontend/src/Components/ItemTable.tsx
+++ src/main/frontend/src/Components/ItemTable.tsx
@@ -6,38 +6,92 @@ import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
-import { Box, Button, Container } from '@mui/material'
+import { Box, Button, Container, TextField } from '@mui/material'
 import axios from 'axios'
+import { parse } from 'path'
+import { DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { DEFAULT_LOCALE } from '@mui/x-date-pickers/locales'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import dayjs from 'dayjs'
 
-const ITEMS_TOKEN_URL = "http://localhost:8080/getall";
+const GETALL_TOKEN_URL = "http://localhost:8080/getall"
+const CREATE_TOKEN_URL = "http://localhost:8080/create"
 
 interface Item {
-  itemId: string;
-  userId: string;
-  title: string;
-  time: string;
-  description: string;
-  importance: string;
-  completed: boolean;
+  itemId: string
+  userId: string
+  title: string
+  time: string
+  description: string
+  importance: string
+  completed: boolean
 }
 
-const ItemTable = () => {
+const ItemTable = (props: any) => {
   const [items, setItems] = useState<Item[]>([])
 
+  const [itemID, setItemID] = useState('')
+  const [userID, setUserID] = useState('')
+  const [title, setTitle] = useState('')
+  const [time, setTime] = useState('')
+  const [description, setDescription] = useState('')
+  const [importance, setImportance] = useState('')
+
+  var token = ''
+
   useEffect(() => {
-    const userToken = localStorage.getItem('userToken');
+    const userToken = localStorage.getItem('userToken')
     if (userToken) {
-      const parsedToken = JSON.parse(userToken);
+      const parsedToken = JSON.parse(userToken)
       if (parsedToken.userId) {
-        axios.post(ITEMS_TOKEN_URL, { userId: parsedToken.userId })
-          .then(res => setItems(res.data))
-          .catch(err => console.log(err))
+        token = parsedToken.userId
+        setUserID(parsedToken.userId)
       }
     }
   }, [])
 
+  useEffect(() => {
+    axios.post(GETALL_TOKEN_URL, { userId: token })
+      .then(res => setItems(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
+  function create(e: any) {
+    e.preventDefault()
+
+    const itemToCreate = {
+      userID: userID,
+      title: title,
+      time: time,
+      description: description,
+      importance: importance
+    }
+
+    axios.post(CREATE_TOKEN_URL, itemToCreate)
+    .then(res => {
+      console.log(res)
+      axios.post(GETALL_TOKEN_URL, { userId: userID })
+        .then(res => setItems(res.data))
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <Container>
+      <form onSubmit={create}>
+        <Box>
+          <TextField label='Title' onChange={e => setTitle(e.target.value)} />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateTimePicker label="Time" defaultValue={dayjs()} onChange={(e) => setTime(e!.format('YYYY-MM-DDTHH:mm:ss'))} />
+          </LocalizationProvider>
+          <TextField label='Description' onChange={e => setDescription(e.target.value)} />
+          <TextField label='Importance' onChange={e => setImportance(e.target.value)} />
+        </Box>
+        <Box>
+          <Button type='submit' variant='contained'>Create</Button>
+        </Box>
+      </form>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
